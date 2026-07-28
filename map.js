c835946a0380be339c9c5d2b069349db18b6ff7d
notatobloxd-stack <notatobloxd@gmail.com>
2026-07-29 04:17:43 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,33 @@
+"use strict";
 
+class JapanMap {
+
+    constructor(id){
+
+        this.map = L.map(id, {
+
+            zoomControl:true,
+
+            attributionControl:true
+
+        });
+
+        this.map.setView(
+            [36.2,138.2],
+            5
+        );
+
+        L.tileLayer(
+            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            {
+                maxZoom:18,
+                attribution:"© OpenStreetMap contributors"
+            }
+        ).addTo(this.map);
+
+    }
+
+}
+
+const japanMap =
+    new JapanMap("japanMap");
