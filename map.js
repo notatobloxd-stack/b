caad180600789ebf1686769704f2ae241de4c1bc
notatobloxd-stack <notatobloxd@gmail.com>
2026-07-29 04:21:48 +0900

    Update map.js

--- map.js
+++ map.js
@@ -30,6 +30,57 @@ class JapanMap {
 
     }
 
+    setEpicenter(quake) {
+
+    // 前回のマーカーを削除
+    if (this.epicenterMarker) {
+        this.map.removeLayer(this.epicenterMarker);
+    }
+
+    this.epicenterMarker = L.marker([
+        quake.latitude,
+        quake.longitude
+    ]).addTo(this.map);
+
+    this.epicenterMarker.bindPopup(`
+        <b>${quake.hypocenter}</b><br>
+        M${quake.magnitude}<br>
+        深さ ${quake.depth} km<br>
+        最大震度 ${this.scaleToText(quake.maxScale)}<br>
+        ${quake.time}
+    `);
+
+    // 地震の場所へ移動
+    this.map.setView(
+        [quake.latitude, quake.longitude],
+        7,
+        {
+            animate: true,
+            duration: 1
+        }
+    );
+
+}
+
+    scaleToText(scale){
+
+    const table = {
+        10:"1",
+        20:"2",
+        30:"3",
+        40:"4",
+        45:"5弱",
+        50:"5強",
+        55:"6弱",
+        60:"6強",
+        70:"7"
+    };
+
+    return table[scale] ?? "?";
+
+}
+    
+
 }
 
 const japanMap = new JapanMap("japanMap");
